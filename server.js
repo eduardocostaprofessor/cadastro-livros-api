381024667810df5201cd7e44fd644e03d4939c22
Eduardo Costa <eduardocostaprofessor@gmail.com>
2019-10-18 21:03:23 -0300

    Adiciona variáveis de ambiente

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
+require('dotenv/config');
 
 const app = express(); //inicia o app
 app.use(express.json())
@@ -9,13 +10,15 @@ app.use(cors())
 
 let porta = process.env.PORT || 3002
 
-mongoose.set('useNewUrlParser', true);
+mongoose.set('useNewUrlParser', true); 
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
+
 // iniciando o DB
 mongoose.connect(
-    'mongodb://localhost:27017/controle-de-livros', 
+    // 'mongodb://localhost:27017/controle-de-livros', 
+    process.env.MONGO_URL, 
     {useNewUrlParser: true} 
     )
     // models
